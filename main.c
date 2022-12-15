43b70904737e06f350fedb54b8c7024238a1de6d
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-15 20:13:27 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//判斷座標位於何處
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    if (x == 0 && y == 0) {
+        printf("Origin\n");
+    }
+    else if (x == 0) {
+        printf("y-axis\n");
+    }
+    else if (y == 0) {
+        printf("x-axis\n");
+    }
+    else if (x > 0 && y > 0) {
+        printf("1st Quadrant\n");
+    }
+    else if (x < 0 && y > 0) {
+        printf("2nd Quadrant\n");
+    }
+    else if (x < 0 && y < 0) {
+        printf("3rd Quadrant\n");
+    }
+    else if (x > 0 && y < 0) {
+        printf("4th Quadrant\n");
+    }
+    return 0;
+}
